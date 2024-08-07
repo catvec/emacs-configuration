164db8a7190feda1abcbf14c5e9fdc4bad8ec216
Noah Huppert <contact@noahh.io>
2024-08-06 22:01:30 -0400

    deleted old contents in prep for doom

--- machines/metis.el
+++
@@ -1,9 +0,0 @@
-;; Set custom path locations
-(setenv "PATH" (concat (getenv "PATH") ":" "/usr/local/bin"))
-(setq ispell-program-name "/usr/local/bin/ispell")
-(add-to-list 'exec-path "/usr/local/bin")
-
-;; Location of Org mode agenda files on this machine
-(setq my-org-agenda-files
-	 (list
-	  "~/Documents/planner.org"))
--- machines/pascal.el
+++
@@ -1,4 +0,0 @@
-;; Set custom path locations
-(setenv "PATH" (concat (getenv "PATH") ":" "/usr/local/bin"))
-(setq ispell-program-name "/usr/bin/aspell")
-(add-to-list 'exec-path "/usr/local/bin")
