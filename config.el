059d4d82c619079cb7b5a872b302d912ad9faa49
Noah Huppert <contact@noahh.io>
2026-05-12 23:39:50 -0400

    rm old copilot extension code

--- config.el
+++ config.el
@@ -118,14 +118,6 @@
            '((emacs-lisp . t)
              (http . t))))
 
-;; Copilot
-;; (use-package! copilot
-;;   :hook (prog-mode . copilot-mode)
-;;   :bind (:map copilot-completion-map
-;;               ("<tab>" . 'copilot-accept-completion)
-;;               ("TAB" . 'copilot-accept-completion)
-;;               ("C-TAB" . 'copilot-accept-completion-by-word)
-;;               ("C-<tab>" . 'copilot-accept-completion-by-word)))
 ;; Zoom in and out
 (load! "snippets/frame-fns.el")
 (load! "snippets/frame-cmds.el")
